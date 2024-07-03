343998667f4e1d672b80faf6e907bc9760e3fbfe
J1nel0 <gabrielps4jf@gmail.com>
2024-07-03 17:33:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let nome = prompt("Qual o seu nome?")
-let anoN = parseInt(prompt("Em que ao vc nasceu?"))
+let anoN = parseInt(prompt("Em que ano vc nasceu?"))
 let idade24 = 2024 - anoN
 document.write(`Olá ${nome}, em 2024 você fará ${idade24}`)
